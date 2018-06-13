8d3f1c6b6cc29698ce7e94d892b5a815df84fb70
Dahele <h_dahele@tss-hr420-dm29.fc.ca>
2018-06-13 14:00:50 -0400

    added in js

--- js/main.js
+++ js/main.js
@@ -2,16 +2,26 @@
 (function() {
 	console.log("js file loaded");
 
-	//Var
+	//Vars
+	var num1 = document.querySelector("#input1");
+	var num2 = document.querySelector("#input2");
+	var equal = document.querySelector("#trigger");
+	var outcome = document.querySelector("#value");
 
-
+	//console.log(num1);
+	//console.log(num2);
+	//console.log(equal);
+	//console.log(outcome);
 
 	//Functions
-
+	function addStuff() {
+		console.log("from addStuff");
+	}
 
 
 
 	//Listeners
+	equal.addEventListener("click", addStuff, false);
 
 
 
